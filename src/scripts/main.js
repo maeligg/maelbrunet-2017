2f9cdc56fbcf7e2bf1156fafad495f9c2efcbb2e
Maël Brunet <maelbrunet@gmail.com>
2017-07-22 23:16:44 +0200

    Add cornify konami code (very important)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
+const unicorns = function () {
+  const pressed = [];
+  const secretCode = 'unicorn';
+
+  window.addEventListener('keyup', (e) => {
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+      cornify_add();
+    }
+  });
+};
+
 window.onload = function () {
   svg4everybody();
+  unicorns();
 };
