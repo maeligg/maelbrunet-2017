6f5c5f3d6c1f1de26909426d6e95b5f5197f265f
Maël Brunet <maelbrunet@gmail.com>
2017-07-24 00:50:05 +0200

    Finish contact page and change logo hover animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,74 +20,20 @@ const toggleOpen = () => {
   });
 };
 
-const plane = () => {
-  const main = document.querySelector('main');
-  const contactPage = main.classList.contains('contact');
-  if (contactPage) {
-
-    // Plane constructor
-    const Plane = function(obj, path) {
-      this.obj = obj;
-      this.pos = 0;
-      this.path = path;
-      this.length = path.getTotalLength();
-      this.speed = 10000;
-      //this.speed = path.getTotalLength() / 1000;
-      this.box = obj.getBBox();
-    };
-
-    Plane.prototype.update = function() {
-      this.pos += this.speed;
-      this.pos = this.pos >= this.length ? 0 : this.pos;
-      this.render();
-    };
-
-    Plane.prototype.pathDir = function(path) {
-      // path direction
-      const pt1 = path.getPointAtLength(this.pos - 2);
-      const pt2 = path.getPointAtLength(this.pos + 2);
-      const angle = Math.atan2(pt1.y - pt2.y, pt1.x - pt2.x) * (180 / Math.PI);
-      return angle;
-    };
-
-    Plane.prototype.render = function() {
-      // as the plane doesn't start at 0,0 we need to calculate its centre
-      const X = +(this.box.x + (this.box.width / 2)).toFixed(1),
-        Y = +(this.box.y + (this.box.height / 2)).toFixed(1);
-      // find out its point along the path, then calculate the new X and Y positions:
-      const mp = this.path.getPointAtLength(this.pos),
-        tX = mp.x - X,
-        tY = mp.y - Y;
-      // get the rotation at the path point:
-      const tR = this.pathDir(this.path) - 180; // adjusted to face the correct direction
-      // apply the new attributes - note: setting X and Y on the rotate is essential if not at 0,0!
-      this.obj.setAttribute('transform', 'translate(' + tX + ', ' + tY + ') rotate(' + tR + ' ' + X + ' ' + Y + ')');
-      this.obj.setAttribute('opacity', 1);
-    };
-
-    // objects
-    const path = document.querySelector('.fly-path');
-    const plane = new Plane(document.querySelector('.plane'), path);
-
-    // animator
-    let raf;
-    let interval = NaN;
-    function animator() {
-      const now = new Date().getTime(),
-        dt = now - (interval || now);
-      interval = now;
-      raf = window.requestAnimationFrame(animator);
-      plane.update(dt); // update the plane on each call
-    }
-
-    window.requestAnimationFrame(animator);
-  }
+const fly = () => {
+  const plane = document.querySelector('.js-plane');
+  plane.addEventListener('click', () => {
+    plane.classList.add('fly');
+    setTimeout(() => {
+      plane.classList.remove('fly');
+    }, 1000);
+  });
 };
 
 window.onload = function () {
   svg4everybody();
   toggleOpen();
-  //plane();
+  fly();
   unicorns();
 };
 
